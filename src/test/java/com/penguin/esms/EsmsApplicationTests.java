753e6102006ab403904310a155528965898e22e7
Zang <bt.hoanggiang@gmail.com>
2023-10-26 03:40:57 +0700

    first commit

---
+++ src/test/java/com/penguin/esms/EsmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.penguin.esms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
