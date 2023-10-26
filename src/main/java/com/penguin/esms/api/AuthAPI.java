17e460cf40ee5283fb298ad893229788bdcf3806
Hoang Hy <hoanghy01122003@gmail.com>
2023-10-26 16:58:20 +0700

    dev: add base entity

---
+++ src/main/java/com/penguin/esms/api/AuthAPI.java
@@ -0,0 +1,13 @@
+package com.penguin.esms.api;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "/auth")
+public class AuthAPI {
+    @GetMapping
+    public ResponseEntity<?> ping() {
+        return ResponseEntity.ok("OKE");
+    }
+}
