1ba95a338ed2746821baf57d9b6f75a03f30418f
bthZang <bt.hoanggiang@gmail.com>
2023-10-27 14:53:20 +0700

    create staff entity

---
+++ src/main/java/com/penguin/esms/components/staff/StaffEntity.java
@@ -0,0 +1,21 @@
+package com.penguin.esms.components.staff;
+
+import com.penguin.esms.entity.NoteEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
+public class StaffEntity extends NoteEntity {
+    private String name;
+    private String phone;
+    private String username;
+    private String password;
+    private String role;
+    private String email;
+    private String citizenId;
+
+    public StaffEntity(String note) {
+        super(note);
+    }
+}
