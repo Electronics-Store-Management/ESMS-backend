0d6fd1cb4b162f7a41a133bc808f41b8e5881f3d
bthZang <bt.hoanggiang@gmail.com>
2023-10-27 14:31:26 +0700

    create category entity

---
+++ src/main/java/com/penguin/esms/components/category/CategoryEntity.java
@@ -0,0 +1,30 @@
+package com.penguin.esms.components.category;
+
+import com.penguin.esms.entity.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
+public class CategoryEntity extends BaseEntity {
+    private String name;
+
+    public CategoryEntity(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "CategoryEntity{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
