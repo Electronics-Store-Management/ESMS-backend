eb6e2e8bd0bd836c1682a0bb44f0f26edd36b464
bthZang <bt.hoanggiang@gmail.com>
2023-10-27 18:24:24 +0700

    config saleBill

---
+++ src/main/java/com/penguin/esms/components/saleBill/SaleBillService.java
@@ -0,0 +1,13 @@
+package com.penguin.esms.components.saleBill;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SaleBillService {
+    private final SaleBillRepo saleBillRepo;
+    @Autowired
+    public SaleBillService(SaleBillRepo saleBillRepo) {
+        this.saleBillRepo = saleBillRepo;
+    }
+}
