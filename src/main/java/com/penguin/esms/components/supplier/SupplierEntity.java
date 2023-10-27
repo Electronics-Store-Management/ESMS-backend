3786ba1e41751e832ad7e795a6ca9abdfcd1c4b0
bthZang <bt.hoanggiang@gmail.com>
2023-10-27 12:16:35 +0700

    create supplier entity

---
+++ src/main/java/com/penguin/esms/components/supplier/SupplierEntity.java
@@ -0,0 +1,66 @@
+package com.penguin.esms.components.supplier;
+
+import com.penguin.esms.entity.BaseEntity;
+import com.penguin.esms.entity.NoteEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
+public class SupplierEntity extends NoteEntity {
+        private String name;
+        private String phone;
+        private String email;
+
+        private String address;
+
+    public SupplierEntity(String note, String name, String phone, String email, String address) {
+        super(note);
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "SupplierEntity{" +
+                "name='" + name + '\'' +
+                ", phone='" + phone + '\'' +
+                ", email='" + email + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
