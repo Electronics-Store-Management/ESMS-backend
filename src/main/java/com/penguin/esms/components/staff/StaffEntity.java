4fbdc957c3b323c9e1cb3fa2c43c532cbe76dc95
bthZang <bt.hoanggiang@gmail.com>
2023-10-27 15:02:53 +0700

    update staff entity

--- src/main/java/com/penguin/esms/components/staff/StaffEntity.java
+++ src/main/java/com/penguin/esms/components/staff/StaffEntity.java
@@ -18,4 +18,84 @@ public class StaffEntity extends NoteEntity {
     public StaffEntity(String note) {
         super(note);
     }
+
+    public StaffEntity(String note, String name, String phone, String username, String password, String role, String email, String citizenId) {
+        super(note);
+        this.name = name;
+        this.phone = phone;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+        this.email = email;
+        this.citizenId = citizenId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCitizenId() {
+        return citizenId;
+    }
+
+    public void setCitizenId(String citizenId) {
+        this.citizenId = citizenId;
+    }
+
+    @Override
+    public String toString() {
+        return "StaffEntity{" +
+                "name='" + name + '\'' +
+                ", phone='" + phone + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", role='" + role + '\'' +
+                ", email='" + email + '\'' +
+                ", citizenId='" + citizenId + '\'' +
+                '}';
+    }
 }
