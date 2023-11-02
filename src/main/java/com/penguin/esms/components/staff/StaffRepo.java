69d445346388ee6a5503e7ee6447c8868dae5fcf
Hoang Hy <hoanghy01122003@gmail.com>
2023-11-02 22:38:55 +0700

    remove redundant files

--- src/main/java/com/penguin/esms/components/staff/StaffRepo.java
+++
@@ -1,8 +0,0 @@
-package com.penguin.esms.components.staff;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StaffRepo extends JpaRepository<StaffEntity, String> {
-}
