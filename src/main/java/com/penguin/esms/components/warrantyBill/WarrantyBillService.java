0b4e56d75a5221bc037a0d3aca9f3f00b8a15c70
bthZang <bt.hoanggiang@gmail.com>
2023-11-05 18:22:33 +0700

    add method checkWarranty

--- src/main/java/com/penguin/esms/components/warrantyBill/WarrantyBillService.java
+++ src/main/java/com/penguin/esms/components/warrantyBill/WarrantyBillService.java
@@ -3,11 +3,21 @@ package com.penguin.esms.components.warrantyBill;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.ZoneId;
+import java.util.Date;
+
 @Service
 public class WarrantyBillService {
     private final WarrantyBillRepo warrantyBillRepo;
     @Autowired
     public WarrantyBillService(WarrantyBillRepo warrantyBillRepo) {
         this.warrantyBillRepo = warrantyBillRepo;
+    }
+    public Boolean checkWarranty(Date buyDate, Period periodTime){
+        if (Period.between(LocalDate.now(), buyDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate()).getMonths() > periodTime.getMonths())
+            return false;
+        return true;
     }
 }
