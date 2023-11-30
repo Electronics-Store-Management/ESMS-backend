f8e590fcd00c6584ff75a90d037533cd0bd297c4
Zang <bt.hoanggiang@gmail.com>
2023-11-30 16:44:18 +0700

    config ManyToOne relationship importProduct to importBill and product

--- src/main/java/com/penguin/esms/components/importProduct/ImportProductEntity.java
+++ src/main/java/com/penguin/esms/components/importProduct/ImportProductEntity.java
@@ -1,87 +1,35 @@
 package com.penguin.esms.components.importProduct;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.penguin.esms.components.importBill.ImportBillEntity;
+import com.penguin.esms.components.product.ProductEntity;
+import com.penguin.esms.components.supplier.SupplierEntity;
 import com.penguin.esms.entity.BaseEntity;
 import com.penguin.esms.entity.NoteEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
 
 @Entity
 @Table
+@Getter
+@Setter
+@RequiredArgsConstructor
 public class ImportProductEntity extends BaseEntity {
-    private String productId;
-    private String importBillId;
-
     private Integer quantity;
     private Integer index;
     private Long price;
     private String unit;
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    @JsonIgnoreProperties(value = {"import_products"})
+    private ProductEntity product;
+    @ManyToOne
+    @JoinColumn(name = "importBill_id")
+    @JsonIgnoreProperties(value = {"import_products"})
+    private ImportBillEntity importBill;
 
-    public ImportProductEntity(String productId,String importBillId, Integer quantity, Integer index, Long price, String unit) {
-        this.productId = productId;
-        this.importBillId = importBillId;
-        this.quantity = quantity;
-        this.index = index;
-        this.price = price;
-        this.unit = unit;
-    }
-
-    public String getImportBillId() {
-        return importBillId;
-    }
-
-    public void setImportBillId(String importBillId) {
-        this.importBillId = importBillId;
-    }
-
-    public String getProductId() {
-        return productId;
-    }
-
-    public void setProductId(String productId) {
-        this.productId = productId;
-    }
-
-    public Integer getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
-    }
-
-    public Integer getIndex() {
-        return index;
-    }
-
-    public void setIndex(Integer index) {
-        this.index = index;
-    }
-
-    public Long getPrice() {
-        return price;
-    }
-
-    public void setPrice(Long price) {
-        this.price = price;
-    }
-
-    public String getUnit() {
-        return unit;
-    }
-
-    public void setUnit(String unit) {
-        this.unit = unit;
-    }
-
-    @Override
-    public String toString() {
-        return "ImportProductEntity{" +
-                "productId='" + productId + '\'' +
-                ", importBillId='" + importBillId + '\'' +
-                ", quantity=" + quantity +
-                ", index=" + index +
-                ", price=" + price +
-                ", unit='" + unit + '\'' +
-                '}';
-    }
 }
