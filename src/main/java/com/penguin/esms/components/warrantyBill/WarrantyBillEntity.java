7041d3f4ac8af6fb13e41023a6dde3ed461b7f87
Zang <bt.hoanggiang@gmail.com>
2023-11-30 13:27:21 +0700

    update config warranty entity

--- src/main/java/com/penguin/esms/components/warrantyBill/WarrantyBillEntity.java
+++ src/main/java/com/penguin/esms/components/warrantyBill/WarrantyBillEntity.java
@@ -3,53 +3,20 @@ package com.penguin.esms.components.warrantyBill;
 import com.penguin.esms.entity.BaseEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 import java.util.Date;
 
 @Entity
 @Table
+@Getter
+@Setter
+@RequiredArgsConstructor
 public class WarrantyBillEntity extends BaseEntity {
     private String staffId;
     private String customerId;
-
     private Date warrantyDate;
 
-    public WarrantyBillEntity(String staffId, String customerId, Date warrantyDate) {
-        this.staffId = staffId;
-        this.customerId = customerId;
-        this.warrantyDate = warrantyDate;
-    }
-
-    public String getStaffId() {
-        return staffId;
-    }
-
-    public void setStaffId(String staffId) {
-        this.staffId = staffId;
-    }
-
-    public String getCustomerId() {
-        return customerId;
-    }
-
-    public void setCustomerId(String customerId) {
-        this.customerId = customerId;
-    }
-
-    public Date getWarrantyDate() {
-        return warrantyDate;
-    }
-
-    public void setWarrantyDate(Date warrantyDate) {
-        this.warrantyDate = warrantyDate;
-    }
-
-    @Override
-    public String toString() {
-        return "WarrantyBillEntity{" +
-                "staffId='" + staffId + '\'' +
-                ", customerId='" + customerId + '\'' +
-                ", warrantyDate=" + warrantyDate +
-                '}';
-    }
 }
--- src/main/java/com/penguin/esms/components/warrantyProduct/WarrantyProductEntity.java
+++ src/main/java/com/penguin/esms/components/warrantyProduct/WarrantyProductEntity.java
@@ -3,10 +3,16 @@ package com.penguin.esms.components.warrantyProduct;
 import com.penguin.esms.entity.NoteEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 import java.util.Date;
 @Entity
 @Table
+@Getter
+@Setter
+@RequiredArgsConstructor
 public class WarrantyProductEntity extends NoteEntity {
     private String staffId;
     private String warrantyBillId;
@@ -15,63 +21,4 @@ public class WarrantyProductEntity extends NoteEntity {
     private Integer quantity;
     private String warrantyContent;
 
-    public WarrantyProductEntity(String note, String warrantyBillId,String staffId, String productId, Integer quantity, String warrantyContent) {
-        super(note);
-        this.warrantyBillId = warrantyBillId;
-        this.staffId = staffId;
-        this.productId = productId;
-        this.quantity = quantity;
-        this.warrantyContent = warrantyContent;
-    }
-
-    public String getWarrantyBillId() {
-        return warrantyBillId;
-    }
-
-    public void setWarrantyBillId(String warrantyBillId) {
-        this.warrantyBillId = warrantyBillId;
-    }
-
-    public String getStaffId() {
-        return staffId;
-    }
-
-    public void setStaffId(String staffId) {
-        this.staffId = staffId;
-    }
-
-    public String getProductId() {
-        return productId;
-    }
-
-    public void setProductId(String productId) {
-        this.productId = productId;
-    }
-
-    public Integer getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
-    }
-
-    public String getWarrantyContent() {
-        return warrantyContent;
-    }
-
-    public void setWarrantyContent(String warrantyContent) {
-        this.warrantyContent = warrantyContent;
-    }
-
-    @Override
-    public String toString() {
-        return "WarrantyProductEntity{" +
-                "staffId='" + staffId + '\'' +
-                ", warrantyBillId='" + warrantyBillId + '\'' +
-                ", productId='" + productId + '\'' +
-                ", quantity=" + quantity +
-                ", warrantyContent='" + warrantyContent + '\'' +
-                '}';
-    }
 }
