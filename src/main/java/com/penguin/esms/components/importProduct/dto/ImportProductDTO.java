84eef8d68bc0c4062b442e68f4bbe2e781fc3b4b
Zang <bt.hoanggiang@gmail.com>
2023-11-30 16:42:03 +0700

    add importProductDTO

---
+++ src/main/java/com/penguin/esms/components/importProduct/dto/ImportProductDTO.java
@@ -0,0 +1,17 @@
+package com.penguin.esms.components.importProduct.dto;
+
+import com.penguin.esms.components.product.ProductEntity;
+
+import java.util.List;
+
+public class ImportProductDTO {
+    private String productId;
+    private String importBillId;
+    private Integer quantity;
+    private Integer index;
+    private Long price;
+    private String unit;
+
+
+
+}
