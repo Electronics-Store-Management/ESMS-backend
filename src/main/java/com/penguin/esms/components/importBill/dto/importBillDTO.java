34504a1222bdc8bceaa4b4bb74183e815393511f
Zang <bt.hoanggiang@gmail.com>
2023-11-30 16:41:50 +0700

    add importBillDTO

---
+++ src/main/java/com/penguin/esms/components/importBill/dto/importBillDTO.java
@@ -0,0 +1,10 @@
+package com.penguin.esms.components.importBill.dto;
+
+import java.util.Date;
+
+public class importBillDTO {
+    private String staffId;
+    private String supplierId;
+    private String paymentMethod;
+    private Date importDate;
+}
