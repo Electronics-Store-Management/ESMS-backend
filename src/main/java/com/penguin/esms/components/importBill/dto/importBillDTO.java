abf06fd5d88b7cf9ba65a5a2e31968df830a0f5d
Zang <bt.hoanggiang@gmail.com>
2023-11-30 17:30:40 +0700

    update importBillDTO

--- src/main/java/com/penguin/esms/components/importBill/dto/importBillDTO.java
+++ src/main/java/com/penguin/esms/components/importBill/dto/importBillDTO.java
@@ -1,10 +1,14 @@
 package com.penguin.esms.components.importBill.dto;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class importBillDTO {
     private String staffId;
     private String supplierId;
     private String paymentMethod;
     private Date importDate;
+    private List<String> importProducts = new ArrayList<>();
+
 }
