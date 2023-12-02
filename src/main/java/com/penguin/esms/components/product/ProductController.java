2211fe26a96d54a38b3088fdf54832a4deec1fa2
Hoang Hy <hoanghy01122003@gmail.com>
2023-12-02 19:09:35 +0700

    dev: update cicd

--- src/main/java/com/penguin/esms/components/product/ProductController.java
+++ src/main/java/com/penguin/esms/components/product/ProductController.java
@@ -2,6 +2,7 @@ package com.penguin.esms.components.product;
 
 import com.penguin.esms.components.product.dto.ProductDTO;
 import com.penguin.esms.services.AmazonS3Service;
+import jakarta.annotation.Nullable;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -34,7 +35,7 @@ public class ProductController {
     }
 
     @PostMapping
-    public ResponseEntity<?> postProduct(@RequestParam MultipartFile photo, @Valid ProductDTO productDTO) throws IOException {
+    public ResponseEntity<?> postProduct(@RequestParam @Nullable MultipartFile photo, @Valid ProductDTO productDTO) throws IOException {
         if (photo != null) {
             String objectURL = amazonS3Service.addFile(photo, productDTO.getName() + "_" + photo.getOriginalFilename());
             productDTO.setPhotoURL(objectURL);
@@ -43,14 +44,15 @@ public class ProductController {
     }
 
     @PutMapping("{id}")
-    public ResponseEntity<?> putProduct(@RequestParam MultipartFile photo, @Valid ProductDTO productDTO, @PathVariable String id) throws IOException {
+    public ResponseEntity<?> putProduct(@RequestParam @Nullable MultipartFile photo, @Valid ProductDTO productDTO, @PathVariable String id) throws IOException {
         return ResponseEntity.ok(service.update(productDTO, id, photo));
     }
 
     @DeleteMapping("{id}")
     public ResponseEntity<?> deleteProduct(@PathVariable String id) {
         ProductEntity product = service.remove(id);
-        List<String> parsedURL = Arrays.stream(product.getPhotoURL().split("/")).toList();
+//        if (product.getPhotoURL() != null)
+//        List<String> parsedURL = Arrays.stream(product.getPhotoURL().split("/")).toList();
 //        amazonS3Service.deleteFile(parsedURL.get(parsedURL.size() - 1));
         return ResponseEntity.ok().build();
     }
