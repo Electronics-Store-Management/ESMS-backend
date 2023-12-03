098d05d55827af3b52ded9e4dd24daf8f4ac2c5f
Zang <bt.hoanggiang@gmail.com>
2023-12-04 04:44:49 +0700

    add controller methods of sale bill

--- src/main/java/com/penguin/esms/components/saleBill/SaleBillController.java
+++ src/main/java/com/penguin/esms/components/saleBill/SaleBillController.java
@@ -1,15 +1,30 @@
 package com.penguin.esms.components.saleBill;
 
+import com.penguin.esms.components.importBill.ImportBillEntity;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(path = "saleBill")
+@RequestMapping(path = "sale")
+@RequiredArgsConstructor
 public class SaleBillController {
     private final SaleBillService saleBillService;
-    @Autowired
-    public SaleBillController(SaleBillService saleBillService) {
-        this.saleBillService = saleBillService;
+    @GetMapping("{id}")
+    public SaleBillEntity get(@PathVariable String id) {
+        return saleBillService.getSaleBill(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<?> post(@RequestBody SaleBillEntity saleBillEntity) {
+        return ResponseEntity.ok(saleBillService.postSaleBill(saleBillEntity));
+    }
+    @DeleteMapping("{id}")
+    public ResponseEntity<?> remove(@PathVariable String id) {
+        SaleBillEntity saleBill = saleBillService.removeSaleBill(id);
+        return ResponseEntity.ok().build();
     }
 }
