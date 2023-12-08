f45f9f0fd051a01114f4631f80f8e3feea6c06d4
Zang <bt.hoanggiang@gmail.com>
2023-12-08 16:15:32 +0700

    add audited warranty bill

--- src/main/java/com/penguin/esms/components/warrantyBill/WarrantyBillEntity.java
+++ src/main/java/com/penguin/esms/components/warrantyBill/WarrantyBillEntity.java
@@ -8,6 +8,8 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import org.hibernate.envers.Audited;
+import org.hibernate.envers.NotAudited;
 
 import java.util.Date;
 import java.util.List;
@@ -17,13 +19,16 @@ import java.util.List;
 @Getter
 @Setter
 @RequiredArgsConstructor
+@Audited
 public class WarrantyBillEntity extends BaseEntity {
     private String staffId;
 //    private String customerId;
     private Date warrantyDate;
+    @NotAudited
     @JsonIgnoreProperties(value = {"warrantyProducts"})
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "warrantyBill")
     private List<WarrantyProductEntity> warrantyProducts;
+    @NotAudited
     @ManyToOne
     @JoinColumn(name = "customer_id")
     @JsonIgnoreProperties(value = {"warrantyBills"})
