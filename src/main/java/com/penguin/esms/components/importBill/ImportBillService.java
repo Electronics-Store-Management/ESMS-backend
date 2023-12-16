36e2f123094f4c3c30dc2f0fce173337c158aa13
Zang <bt.hoanggiang@gmail.com>
2023-12-17 05:42:03 +0700

    get all history of import bill by id

--- src/main/java/com/penguin/esms/components/importBill/ImportBillService.java
+++ src/main/java/com/penguin/esms/components/importBill/ImportBillService.java
@@ -6,8 +6,17 @@ import com.penguin.esms.components.product.ProductEntity;
 import com.penguin.esms.components.product.dto.ProductDTO;
 import com.penguin.esms.components.staff.StaffEntity;
 import com.penguin.esms.components.supplier.SupplierEntity;
+import com.penguin.esms.components.warrantyBill.WarrantyBillEntity;
+import com.penguin.esms.components.warrantyBill.dto.WarrantyBillDTO;
 import com.penguin.esms.entity.Error;
+import com.penguin.esms.envers.AuditEnversInfo;
+import com.penguin.esms.envers.AuditEnversInfoRepo;
+import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.envers.AuditReader;
+import org.hibernate.envers.AuditReaderFactory;
+import org.hibernate.envers.query.AuditEntity;
+import org.hibernate.envers.query.AuditQuery;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Service;
@@ -16,14 +25,13 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.io.IOException;
 import java.security.Principal;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
 public class ImportBillService {
+    private final EntityManager entityManager;
+    private final AuditEnversInfoRepo auditEnversInfoRepo;
     private final ImportBillRepo importBillRepo;
     public ImportBillEntity getImportBill(String importBillId) {
         Optional<ImportBillEntity> importBill = importBillRepo.findById(importBillId);
@@ -37,5 +45,33 @@ public class ImportBillService {
         StaffEntity staff = (StaffEntity) ((UsernamePasswordAuthenticationToken) connectedUser).getPrincipal();
         importBillEntity.setStaffId(staff.getId());
         return importBillRepo.save(importBillEntity);
+    }
+
+    public List<?> getRevisions(String id) {
+        AuditReader auditReader = AuditReaderFactory.get(entityManager);
+
+        AuditQuery query = auditReader.createQuery()
+                .forRevisionsOfEntity(ImportBillEntity.class, true, true)
+                .add(AuditEntity.id().eq(id))
+                .addProjection(AuditEntity.revisionNumber())
+                .addProjection(AuditEntity.property("staffId"))
+                .addProjection(AuditEntity.property("supplierId"))
+                .addProjection(AuditEntity.property("paymentMethod"))
+                .addProjection(AuditEntity.revisionType())
+                .addOrder(AuditEntity.revisionNumber().desc());
+
+        List<AuditEnversInfo> audit = new ArrayList<AuditEnversInfo>();
+        List<Object[]> objects = query.getResultList();
+        for(int i=0; i< objects.size();i++){
+            Object[] objArray = objects.get(i);
+            Optional<AuditEnversInfo> auditEnversInfoOptional = auditEnversInfoRepo.findById((int) objArray[0]);
+            if (auditEnversInfoOptional.isPresent()) {
+                AuditEnversInfo auditEnversInfo = auditEnversInfoOptional.get();
+                ImportBillDTO dto = new ImportBillDTO((String) objArray[1],  (String) objArray[2], (String) objArray[3]);
+                auditEnversInfo.setRevision(dto);
+                audit.add(auditEnversInfo);
+            }
+        }
+        return audit;
     }
 }
