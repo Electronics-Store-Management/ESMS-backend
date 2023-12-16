b28ddc469d11de9d4760dfef69a67efa956726ab
Zang <bt.hoanggiang@gmail.com>
2023-12-16 09:30:50 +0700

    add constructor in entity of product

--- src/main/java/com/penguin/esms/components/product/ProductEntity.java
+++ src/main/java/com/penguin/esms/components/product/ProductEntity.java
@@ -50,6 +50,18 @@ public class ProductEntity extends BaseEntity {
     private Boolean isAvailable = true;
     private String photoURL;
 
+    public ProductEntity(String id , String name, CategoryEntity category, String unit, Long price, Integer quantity, Integer warrantyPeriod, Boolean isAvailable, String photoURL){
+        this.setId(id);
+        this.name = name;
+        this.category = category;
+        this.unit = unit;
+        this.price = price;
+        this.quantity = quantity;
+        this.warrantyPeriod = warrantyPeriod;
+        this.isAvailable = isAvailable;
+        this.photoURL = photoURL;
+    }
+
     @JsonIgnoreProperties(value = {"importProducts"})
     @NotAudited
     @JsonIgnore
