3249bec46a3e81820635073a658a43d8e94d6d40
Zang <bt.hoanggiang@gmail.com>
2023-12-18 02:03:02 +0700

    update check history by staff : get more information

---
+++ src/main/java/com/penguin/esms/envers/dto/AuditEnverInfoDTO.java
@@ -0,0 +1,23 @@
+package com.penguin.esms.envers.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.envers.RevisionType;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class AuditEnverInfoDTO {
+    private String id;
+    private String name;
+    private RevisionType revType;
+    private Integer revNumber;
+
+    public AuditEnverInfoDTO(String id, String name, RevisionType revType, Integer revNumber) {
+        this.id = id;
+        this.name = name;
+        this.revType = revType;
+        this.revNumber = revNumber;
+    }
+}
