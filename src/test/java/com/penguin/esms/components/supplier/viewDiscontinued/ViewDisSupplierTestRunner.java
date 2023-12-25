601f1070365de8d3b9e1bb4ea0dbe028cc788510
Zang <bt.hoanggiang@gmail.com>
2023-12-25 16:57:20 +0700

    200 ok test get dis supplier by id

--- src/test/java/com/penguin/esms/components/supplier/viewDiscontinued/ViewDisSupplierTestRunner.java
+++ src/test/java/com/penguin/esms/components/supplier/viewDiscontinued/ViewDisSupplierTestRunner.java
@@ -3,9 +3,9 @@ package com.penguin.esms.components.supplier.viewDiscontinued;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.penguin.esms.EsmsApplication;
 import com.penguin.esms.components.authentication.responses.AuthenticationResponse;
-import com.penguin.esms.components.customer.CustomerEntity;
-import com.penguin.esms.components.customer.CustomerRepo;
 import com.penguin.esms.components.staff.StaffRepository;
+import com.penguin.esms.components.supplier.SupplierEntity;
+import com.penguin.esms.components.supplier.SupplierRepo;
 import com.penguin.esms.utils.TestCase;
 import com.penguin.esms.utils.TestService;
 import com.penguin.esms.utils.TestUtils;
@@ -41,12 +41,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @TestPropertySource(
         locations = "classpath:application-test.properties")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-class ViewBannedCustomerTestRunner {
+class ViewDisSupplierTestRunner {
 
     @Autowired
     private MockMvc mockMvc;
     @Autowired
-    private CustomerRepo customerRepo;
+    private SupplierRepo supplierRepo;
 
     @Autowired
     private StaffRepository staffRepository;
@@ -57,26 +57,26 @@ class ViewBannedCustomerTestRunner {
     private TestService testService;
 
     private AuthenticationResponse authenticationResponse;
-    private CustomerEntity customer;
+    private SupplierEntity supplier;
 
 
     @Autowired
-    public ViewBannedCustomerTestRunner(MockMvc mockMvc, ObjectMapper objectMapper, CustomerRepo customerRepo, StaffRepository staffRepository, TestService testService) {
+    public ViewDisSupplierTestRunner(MockMvc mockMvc, ObjectMapper objectMapper, SupplierRepo supplierRepo, StaffRepository staffRepository, TestService testService) {
         this.mockMvc = mockMvc;
         this.objectMapper = objectMapper;
-        this.customerRepo = customerRepo;
+        this.supplierRepo = supplierRepo;
         this.staffRepository = staffRepository;
         this.testService = testService;
     }
 
     public static List<TestCase> testData() throws IOException {
-        return TestUtils.readTestDataFromCsv("src\\test\\java\\com\\penguin\\esms\\components\\customer\\view\\test-cases.csv", new ArrayList<>(List.of("name")), new ArrayList<>(List.of("length", "status")));
+        return TestUtils.readTestDataFromCsv("src\\test\\java\\com\\penguin\\esms\\components\\supplier\\viewDiscontinued\\test-cases.csv", new ArrayList<>(List.of("name")), new ArrayList<>(List.of("length", "status")));
     }
 
     @ParameterizedTest
     @MethodSource("testData")
-    public void shouldViewBannedCustomer(TestCase testCase) throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/customer?name=" + testCase.getInput().get("name"))
+    public void shouldViewCustomer(TestCase testCase) throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/supplier?name=" + testCase.getInput().get("name"))
                         .header("Authorization", "Bearer " + authenticationResponse.getAccessToken())
                 )
                 .andExpect(status().is(Integer.parseInt(testCase.getExpected().get("status"))))
@@ -86,11 +86,12 @@ class ViewBannedCustomerTestRunner {
 
     @BeforeAll
     public void setup() throws Exception {
-        CustomerEntity cutomerr = new CustomerEntity();
-        cutomerr.setName("This is customer");
-        cutomerr.setIsStopped(true);
+        SupplierEntity entity = new SupplierEntity();
+        entity.setName("This is a supplier");
+        entity.setIsStopped(true);
+
         CompletableFuture<Void> setupEntity = CompletableFuture.runAsync(() -> {
-            this.customer = customerRepo.save(cutomerr);
+            this.supplier = supplierRepo.save(entity);
             try {
                 authenticationResponse = testService.getAuthenticationInfo();
             } catch (Exception e) {
@@ -98,12 +99,13 @@ class ViewBannedCustomerTestRunner {
             }
         });
 
+
         setupEntity.join();
     }
 
     @AfterAll
     public void cleanup() {
-        customerRepo.deleteById(this.customer.getId());
+        supplierRepo.deleteById(this.supplier.getId());
         staffRepository.deleteById(this.authenticationResponse.getId());
     }
 }
